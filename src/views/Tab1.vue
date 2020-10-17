db74cd494738d86b72b5c67ce1a0c75ab0ba9cf3
Petr Hnyk <[hnyk.petr@centrum.cz]>
2020-10-17 13:41:11 +0200

    added Leaflet to tab 1

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -6,23 +6,51 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <div style="height: 100%; width: 100%;">
+        <l-map
+            v-model="zoom"
+            v-model:zoom="zoom"
+            :center="[50.44176389056172, 14.08721923828125]"
+            @move="log('move')"
+        >
+          <l-tile-layer
+              url="https://m1.mapserver.mapy.cz/turist-m/{z}-{x}-{y}"
+          ></l-tile-layer>
+          <l-marker
+              :lat-lng="[50.44176389056172, 14.08721923828125]"
+          ></l-marker>
+        </l-map>
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import "leaflet/dist/leaflet.css";
+
 
 export default  {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    LMap,
+    LTileLayer,
+    LMarker},
+  data() {
+    return {
+      zoom: 2,
+    };
+  },
+  methods: {
+    log(a) {
+      console.log(a);
+    },
+  },
 }
 </script>
